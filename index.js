ade32199a130b47b7e661c78bd0598417b58dffb
Esteban <esteban.ouvre@gmail.com>
2023-03-01 12:32:37 +0100

    Correction: middleware for request header and verify token

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+mongoose.set("strictQuery", true);
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
